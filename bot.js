16be72f348a5defe728f49ca43e7af62e9a9d2ea
kahrbayt22 <40933186+kahrbayt22@users.noreply.github.com>
2018-07-09 12:29:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ const { Client, Util } = require('discord.js');
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 const YouTube = require('simple-youtube-api');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const youtube = new YouTube("AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4");
 const queue = new Map();
 const client = new Discord.Client();
 
